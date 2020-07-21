fb549fc690992302ae15c40959f157cc5bebde4f
mharb <mohamed.harb@cegedim.com>
2020-07-21 18:09:54 +0200

    Added batch file to start keycloak

---
+++ keycloak/startKeycloak.bat
@@ -0,0 +1,1 @@
+docker run -p 8180:8080 -e KEYCLOAK_USER_FILE=/tmp/user.txt -e KEYCLOAK_PASSWORD_FILE=/tmp/password.txt -e KEYCLOAK_IMPORT=/tmp/sample-realm.json -v ${PWD}/files:/tmp -d jboss/keycloak
