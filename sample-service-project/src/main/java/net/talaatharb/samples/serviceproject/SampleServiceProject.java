235cbabc8ebe1e63f25b76c7374c1f12434810db
mharb <mohamed.harb@cegedim.com>
2020-07-27 14:46:01 +0200

    Removed unused code and configuration

--- sample-service-project/src/main/java/net/talaatharb/samples/serviceproject/SampleServiceProject.java
+++ sample-service-project/src/main/java/net/talaatharb/samples/serviceproject/SampleServiceProject.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 public class SampleServiceProject {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		SpringApplication.run(SampleServiceProject.class, args);
 	}
 
--- sample-service-project/src/main/java/net/talaatharb/samples/serviceproject/api/CrudController.java
+++ sample-service-project/src/main/java/net/talaatharb/samples/serviceproject/api/CrudController.java
@@ -8,24 +8,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
+import net.talaatharb.samples.serviceproject.microservice.SecuredMicroService;
 import net.talaatharb.samples.serviceproject.microservice.UnsecuredMicroService;
 import net.talaatharb.samples.serviceproject.microservice.dto.ItemDto;
+import net.talaatharb.samples.serviceproject.microservice.dto.UserDto;
 
 @RestController
 @RequestMapping("/api")
 @Slf4j
 public class CrudController {
-	
+
 	private static final String UNSECURED_SERVICE_NAME = "unsecured";
-	
+
+	private static final String SECURED_SERVICE_NAME = "secured";
+
 	private static final String FIND_ALL_MESSAGE = "Find all from {} called";
-	
+
 	@Autowired
 	private UnsecuredMicroService unsecuredService;
 
+	@Autowired
+	private SecuredMicroService securedService;
+
 	@GetMapping(path = "/unsecured")
-	public List<ItemDto> findAllFromUnsecured(){
+	public List<ItemDto> findAllFromUnsecured() {
 		log.info(FIND_ALL_MESSAGE, UNSECURED_SERVICE_NAME);
 		return unsecuredService.findAll();
+	}
+
+	@GetMapping(path = "/secured")
+	public List<UserDto> findAllFromSecured() {
+		log.info(FIND_ALL_MESSAGE, SECURED_SERVICE_NAME);
+		return securedService.findAll();
 	}
 }
