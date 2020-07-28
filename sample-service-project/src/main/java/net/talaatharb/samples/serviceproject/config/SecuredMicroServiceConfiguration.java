1628f6d0a31c44658e0c50d57d10b65797706cb1
mharb <mohamed.harb@cegedim.com>
2020-07-28 12:42:52 +0200

    Reduced code to contact secured microservice to the essential parts

--- sample-service-project/src/main/java/net/talaatharb/samples/serviceproject/config/SecuredMicroServiceConfiguration.java
+++ sample-service-project/src/main/java/net/talaatharb/samples/serviceproject/config/SecuredMicroServiceConfiguration.java
@@ -1,21 +1,14 @@
 package net.talaatharb.samples.serviceproject.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.web.client.RestTemplate;
 
 import feign.RequestInterceptor;
 
-@EnableOAuth2Sso
 public class SecuredMicroServiceConfiguration {
 
 	@Value("${keycloak.token-uri}")
@@ -33,34 +26,19 @@ public class SecuredMicroServiceConfiguration {
 		this.oAuth2ClientContext = new DefaultOAuth2ClientContext();
 	}
 
-	@Value("${keycloak.authorization-uri}")
-	private String userAuthorizationUri;
-
-	 @Bean
-	public RequestInterceptor oAuth2RequestInterceptor() {
+	@Bean
+	public RequestInterceptor oAuth2RequestInterceptor(
+			OAuth2ProtectedResourceDetails resourceDetails) {
 		return new SecuredMicroServiceInterceptor(oAuth2ClientContext,
-				resource());
+				resourceDetails);
 	}
 
 	@Bean
-	public RestTemplate oAuthRestTemplate(
-			OAuth2ProtectedResourceDetails resource) {
-		OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resource,
-				oAuth2ClientContext);
-
-		return restTemplate;
-	}
-
-	@Bean
-	public OAuth2ProtectedResourceDetails resource() {
-		ClientCredentialsResourceDetails resource = new ClientCredentialsResourceDetails();
-		resource.setAccessTokenUri(accessTokenUri);
-		// resource.setUserAuthorizationUri(userAuthorizationUri);
-		resource.setClientId(clientId);
-		resource.setClientSecret(clientSecret);
-		resource.setGrantType(
-				AuthorizationGrantType.CLIENT_CREDENTIALS.getValue());
-		resource.setScope(Arrays.asList("roles"));
-		return resource;
+	public OAuth2ProtectedResourceDetails resourceDetails() {
+		ClientCredentialsResourceDetails resourceDetails = new ClientCredentialsResourceDetails();
+		resourceDetails.setAccessTokenUri(accessTokenUri);
+		resourceDetails.setClientId(clientId);
+		resourceDetails.setClientSecret(clientSecret);
+		return resourceDetails;
 	}
 }
--- sample-service-project/src/main/java/net/talaatharb/samples/serviceproject/config/SecuredMicroServiceInterceptor.java
+++ sample-service-project/src/main/java/net/talaatharb/samples/serviceproject/config/SecuredMicroServiceInterceptor.java
@@ -1,106 +1,17 @@
 package net.talaatharb.samples.serviceproject.config;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.http.AccessTokenRequiredException;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.client.resource.UserRedirectRequiredException;
-import org.springframework.security.oauth2.client.token.AccessTokenProvider;
-import org.springframework.security.oauth2.client.token.AccessTokenProviderChain;
-import org.springframework.security.oauth2.client.token.AccessTokenRequest;
-import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider;
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
-import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitAccessTokenProvider;
-import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordAccessTokenProvider;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
 
 public class SecuredMicroServiceInterceptor
 		extends
 			OAuth2FeignRequestInterceptor {
 
-	@Value("${keycloak.token-uri}")
-	private String accessTokenUri;
-
-	@Value("${spring.security.oauth2.client.registration.secured.client-id}")
-	private String clientId;
-
-	@Value("${spring.security.oauth2.client.registration.secured.client-secret}")
-	private String clientSecret;
-
-	private final OAuth2ClientContext oAuth2ClientContext;
-
-	@Value("${keycloak.authorization-uri}")
-	private String userAuthorizationUri;
-
-	private final OAuth2ProtectedResourceDetails resource;
-
 	public SecuredMicroServiceInterceptor(
 			OAuth2ClientContext oAuth2ClientContext,
-			OAuth2ProtectedResourceDetails resource) {
-		super(oAuth2ClientContext, resource);
-		this.oAuth2ClientContext = oAuth2ClientContext;
-		this.resource = resource;
-		acquireAccessToken(resource);
-	}
-
-	private AccessTokenProvider accessTokenProvider() {
-		ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-		List<AccessTokenProvider> accessTokenProviders = Arrays.asList(
-				new AuthorizationCodeAccessTokenProvider(),
-				new ImplicitAccessTokenProvider(),
-				new ResourceOwnerPasswordAccessTokenProvider(),
-				new ClientCredentialsAccessTokenProvider());
-
-		AccessTokenProviderChain result = new AccessTokenProviderChain(
-				accessTokenProviders);
-		result.setRequestFactory(requestFactory);
-
-		return result;
-	}
-
-	@Override
-	protected OAuth2AccessToken acquireAccessToken() {
-		return acquireAccessToken(resource);
-	}
-
-	/**
-	 * @see OAuth2FeignRequestInterceptor#acquireAccessToken()
-	 */
-	protected OAuth2AccessToken acquireAccessToken(
-			OAuth2ProtectedResourceDetails resource)
-			throws UserRedirectRequiredException {
-		AccessTokenRequest tokenRequest = oAuth2ClientContext
-				.getAccessTokenRequest();
-		if (tokenRequest == null) {
-			throw new AccessTokenRequiredException(
-					"Cannot find valid context on request for resource '"
-							+ resource.getId() + "'.",
-					resource);
-		}
-		String stateKey = tokenRequest.getStateKey();
-		if (stateKey != null) {
-			tokenRequest.setPreservedState(
-					oAuth2ClientContext.removePreservedState(stateKey));
-		}
-		OAuth2AccessToken existingToken = oAuth2ClientContext.getAccessToken();
-		if (existingToken != null) {
-			oAuth2ClientContext.setAccessToken(existingToken);
-		}
-		OAuth2AccessToken obtainableAccessToken;
-		obtainableAccessToken = accessTokenProvider()
-				.obtainAccessToken(resource, tokenRequest);
-		if (obtainableAccessToken == null
-				|| obtainableAccessToken.getValue() == null) {
-			throw new IllegalStateException(
-					" Access token provider returned a null token, which is illegal according to the contract.");
-		}
-		oAuth2ClientContext.setAccessToken(obtainableAccessToken);
-		return obtainableAccessToken;
+			OAuth2ProtectedResourceDetails resourceDetails) {
+		super(oAuth2ClientContext, resourceDetails);
+		acquireAccessToken();
 	}
 }
